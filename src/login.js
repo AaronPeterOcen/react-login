ae24c8548d6d3ef2be7a78b91195d02898f6bb3f
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-13 11:55:16 +0300

    creating the login page

--- src/login.js
+++ src/login.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-  return <div>Login</div>;
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    //
+  };
+
+  return (
+    <div className={"mainContainer"}>
+      <div className={"titleContainer"}>
+        <div>Login</div>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={email}
+          placeholder="Enter email here"
+          className={"inputBox"}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log in"}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
