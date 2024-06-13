609bfb06516f0bb41b7f62437c9b3a7c041cb427
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-13 11:42:20 +0300

    updating App.js to setup page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Home from "./home";
+import Login from "./login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+        </Routes>
+      </BrowserRouter>
+      {/* <Home /> */}
     </div>
   );
 }
--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const Home = (props) => {
   const { loggedIn, email } = props;
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
   const onButtonClick = () => {
     //
